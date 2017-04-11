7b1ace22260d92299770fb6fb75f2e3768cc1aca
Marcel Borsten <marcel@impart.nl>
2017-04-11 13:08:27 +0200

    Added tests

--- AfterX.podspec
+++ AfterX.podspec
@@ -1,14 +1,7 @@
 Pod::Spec.new do |s|
   s.name             = 'AfterX'
   s.version          = '0.3.0'
-  s.summary          = 'AfterX helps you keep track of things you only want to do once, or after x amount of times, during the application lifetime'
-
-# This description is used to generate tags and improve search results.
-#   * Think: What does it do? Why did you write it? What is the focus?
-#   * Try to keep it short, snappy and to the point.
-#   * Write the description between the DESC delimiters below.
-#   * Finally, don't worry about the indent, CocoaPods strips it!
-
+  s.summary          = 'Do something once, after x amount of times'
   s.description      = <<-DESC
 AfterX is ideal for keeping track of onboarding, tips, etc. Want to show a tip the first time a user sees a view?: `AfterX.doOnce()`. Want to ask the user for a glaring review after succeeding a task 5 times?: `AfterX.do(task: "once_after_five", after: 5) { self.askForReview() }`
                        DESC
@@ -22,5 +15,4 @@ AfterX is ideal for keeping track of onboarding, tips, etc. Want to show a tip t
   s.ios.deployment_target = '8.0'
 
   s.source_files = 'AfterX/Classes/**/*'
-  
 end
--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -3,27 +3,49 @@ import XCTest
 import AfterX
 
 class Tests: XCTestCase {
-    
+
+    private let onceTask = "once"
+    private let onceAfterTask = "once_after"
+
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        AfterX.resetAll()
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+
+        AfterX.resetAll()
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
+    func testOnceFireFirstTime() {
+        var didOnce = false
+
+        AfterX.doOnce(task: onceTask) { 
+            didOnce = true
         }
+        XCTAssertTrue(didOnce)
     }
-    
+
+    func testOnceDoesntFireSecondTime() {
+        var didOnce = false
+
+        AfterX.doOnce(task: onceTask) {
+            didOnce = true
+        }
+        XCTAssertTrue(didOnce)
+
+        didOnce = false
+
+        AfterX.doOnce(task: onceTask) { 
+            didOnce = true
+        }
+
+        XCTAssertFalse(didOnce)
+    }
+
+    func testDoOnceAfter() {
+
+
+    }
 }
